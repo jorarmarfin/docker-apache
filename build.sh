0e6987dad559d822360402074a0913cc30ead122
Luis mayta <luis.mayta@gmail.com>
2019-01-03 14:48:56 -0500

    build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t jorarmarfin/apache-php:7.2 .
 
-docker run --name srv-apache -d -p 8181:80 lfmc/iso-apache
+docker run --name srv-apache -d -p 8181:80 jorarmarfin/apache-php:7.2-opcache 
