e22679bee30ac8ab6a4d8d616799e8ad90782d4b
Luis mayta <luis.mayta@gmail.com>
2019-01-11 09:54:12 -0500

    carpeta backup

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 docker build -t jorarmarfin/apache-php:7.2 .
-=======
 docker build -t jorarmarfin/apache-php:7.0 .
->>>>>>> 4eca16d8abc42e19feca0cadc3cc0a1eac49c7e6
 
 docker run --name srv-apache -d -p 8181:80 jorarmarfin/apache-php:7.2-opcache 
