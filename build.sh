bf7d89f0581e4fcb7e6856a7dec9e36b371c0c43
Luis mayta <luis.mayta@gmail.com>
2019-09-09 13:01:30 -0500

    Sin mysql-client

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ docker build -t jorarmarfin/apache-php:latest .
 
 docker build -t jorarmarfin/apache-php:7.0 .
 
-docker run --name srv-apache -d -p 8181:80 jorarmarfin/apache-php:7.2-opcache 
+docker run --name srv-apache -d -p 8181:80 jorarmarfin/apache-php:latest
