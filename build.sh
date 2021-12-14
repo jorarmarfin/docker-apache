92407f0e934a1f9f671e5af4ac8d01f5bae56e37
Luis Mayta <luis.mayta@gmail.com>
2021-12-14 10:41:20 -0500

    Php 7.4.22 drive sqlsrv, soap

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 docker build -t jorarmarfin/apache-php:tmp .
+docker build -t jorarmarfin/apache-php:tmp1 .
 docker build -t jorarmarfin/apache-php:7.2 .
 docker build -t drinuxsac/qcrm2:1.0 .
 
 docker build -t jorarmarfin/apache-php:7.4.22 .
 
+docker run --name srv-apache-tmp -d -p 9001:80 jorarmarfin/apache-php:tmp
 docker run --name srv-apache-tmp -d -p 9001:80 jorarmarfin/apache-php:sqlsrv
 docker run --name srv-apache -d -p 8181:80 jorarmarfin/apache-php:latest
 
@@ -11,7 +13,7 @@ drm srv-apache
 
 docker tag fe62c6d62a2f jorarmarfin/apache-php:latest
 
-
+# Debug
 zend_extension=/usr/local/lib/php/extensions/no-debug-non-zts-20160303/xdebug.so
 xdebug.default_enable=1
 xdebug.remote_enable=1
@@ -22,3 +24,36 @@ xdebug.remote_host=host.docker.internal
 xdebug.idekey=VSCODE
 xdebug.remote_autostart=1
 xdebug.remote_log=/var/www/xdebug.log
+
+
+#Instalar SOAP
+RUN apt-get update && \
+    apt-get install -y libxml2-dev \
+    && apt-get install php${PHP_VERSION}-soap -y
+
+# Installation of compass
+RUN apt-get update && \
+    apt-get install -y git ruby-full && \
+    gem install --no-rdoc --no-ri sass -v 3.4.22 && \
+    gem install --no-rdoc --no-ri compass && \
+    gem install susy
+
+# Microsoft SQL Server Prerequisites
+RUN apt-get update \
+    && apt-get install -y gnupg gnupg1 gnupg2 \
+    && curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
+    && curl https://packages.microsoft.com/config/debian/10/prod.list \
+        > /etc/apt/sources.list.d/mssql-release.list \
+    && apt-get install -y --no-install-recommends \
+        locales \
+        apt-transport-https \
+    && echo "en_US.UTF-8 UTF-8" > /etc/locale.gen \
+    && locale-gen \
+    && apt-get update \
+    && apt-get -y --no-install-recommends install \
+        unixodbc-dev \
+        msodbcsql17 \
+        mssql-tools \
+        && echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc 
+RUN pecl install sqlsrv pdo_sqlsrv \
+    && docker-php-ext-enable sqlsrv pdo_sqlsrv 
